2b67f4dbff3b65fca2d546c7fdc16cb0e3dc40e2
SylvesterKwon <asix71@gmail.com>
2022-04-18 15:45:57 +0900

    FEAT: mathjax support

--- wiki/templatetags/wiki_filter.py
+++ wiki/templatetags/wiki_filter.py
@@ -22,12 +22,16 @@ def mark(value):
             "base_url": "/wiki/",
         },
         "toc":{
-            "title": "목차",
+            "title": "Index",
+        },
+        "md4mathjax":{
+            "auto_insert": "False",
+            "tag_class": "math",
         }
     }
 
     # bleach settings
-    allowed_tags=['p', 'a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'strong', 'ul', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'pre', 'div', 'span']
+    allowed_tags=['p', 'a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'strong', 'ul', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'pre', 'div', 'span', 'img']
     allowed_attributes={'a': ['href', 'title'], 'abbr': ['title'], 'acronym': ['title'], 'div': ['class']}
     
     return mark_safe(bleach.clean(markdown.markdown(value, extensions=extensions, extension_configs=extension_configs),
